f6afbb85efcdc2a76073e5fe193cf8347e4d3948
Greg Morgan <gregm9999@gmail.com>
2018-11-08 01:50:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do |holiday, supplies|
     supplies
-  end
+  end.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
