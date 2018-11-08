aff2285eccd391ade6c8d61d110df0bb80cafdc6
Greg Morgan <gregm9999@gmail.com>
2018-11-08 01:50:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,14 +42,9 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     holiday_hash[season][holiday_name]=supply_array
 end
 
-# def all_winter_holiday_supplies(holiday_hash)
-#   # return an array of all of the supplies that are used in the winter season
-#   holiday_hash[:winter].values.flatten
-# end
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].map do |holiday, supplies|
-    supplies
-  end.flatten
+  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
